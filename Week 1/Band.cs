14f9ef0b64c898a97308152e55308a0df1a3b62a
S00189006 <S00189006@LAB-B1042-03>
2020-01-21 12:33:31 +0000

    Week 1

---
+++ Week 1/Band.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_1
+{
+    class Band : IComparable
+    {
+        //Properties
+        public string BandName { get; set; }
+        public int YearFormed { get; set; }
+        public string BandMembers { get; set; }
+
+        //Constructors
+        public Band(string bandName, int yearFormed, string bandMembers)
+        {
+            BandName = bandName;
+            YearFormed = yearFormed;
+            BandMembers = bandMembers;
+        }
+
+        public Band()
+        {
+        }
+
+        //Methods
+
+        public int CompareTo(object obj)
+        {
+            Band that = (Band)obj;
+
+            //Compare one Activity Date to another 
+            return this.BandName.CompareTo(that.BandName);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {ActivityDate.ToShortDateString()}";
+        }
+    }
+}
--- Week 1/MainWindow.xaml.cs
+++ Week 1/MainWindow.xaml.cs
@@ -20,9 +20,68 @@ namespace Week_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Band> bands = new List<Band>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Create Bands
+            Band b1 = new Band()
+            {
+                BandName = "sfdsfsdf",
+                YearFormed = 2010,
+                BandMembers = "sdfasdf"
+            };
+
+            Band b2 = new Band()
+            {
+                BandName = "sfdsfsdf",
+                YearFormed = 2010,
+                BandMembers = "sdfasdf"
+            };
+
+            Band b3 = new Band()
+            {
+                BandName = "sfdsfsdf",
+                YearFormed = 2010,
+                BandMembers = "sdfasdf"
+            };
+
+            Band b4 = new Band()
+            {
+                BandName = "sfdsfsdf",
+                YearFormed = 2010,
+                BandMembers = "sdfasdf"
+            };
+
+            Band b5 = new Band()
+            {
+                BandName = "sfdsfsdf",
+                YearFormed = 2010,
+                BandMembers = "sdfasdf"
+            };
+
+            Band b6 = new Band()
+            {
+                BandName = "sfdsfsdf",
+                YearFormed = 2010,
+                BandMembers = "sdfasdf"
+            };
+
+            //Display list in the list box
+            bands.Add(b1);
+            bands.Add(b2);
+            bands.Add(b3);
+            bands.Add(b4);
+            bands.Add(b5);
+            bands.Add(b6);
+
+            //Display the activities in the listbox
+            lbxBands.ItemsSource = null;
+            lbxBands.ItemsSource = bands;
         }
     }
 }
